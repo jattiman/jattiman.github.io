d4703dc6f4a5386ed7b2e5a293ce82a8d092ce8f
John Atti <33670809+jattiman@users.noreply.github.com>
2023-06-24 16:15:57 -0400

    oh noes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/jattiman.github.io/", // for github pages
+  // base: "/jattiman.github.io/", // for github pages I GUESS
+  base: "/", // for github pages I GUESS
   plugins: [react()],
 })
