af13a3e80a07a9b9adf068501c2877dbfd6ec590
John Atti <33670809+jattiman@users.noreply.github.com>
2023-06-24 19:23:50 -0400

    ¯\_(ツ)_/¯
    Yolo

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig(({command}) => {
   const config = {
-    plugins: [react()],
     base: "/", // for github pages I GUESS
-    // base: "/jattiman.github.io/", // for github pages I GUESS
+    plugins: [react()],
   }
   if (command !== 'serve') {
     config.base = '/';
