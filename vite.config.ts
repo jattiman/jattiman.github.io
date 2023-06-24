b1c9f55d2e061188a1e7754f7b3fabdbf189b955
John Atti <33670809+jattiman@users.noreply.github.com>
2023-06-24 16:30:25 -0400

    I'll get the hang of this.
    Or not.

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // base: "/jattiman.github.io/", // for github pages I GUESS
-  base: "/", // for github pages I GUESS
-  plugins: [react()],
+export default defineConfig(({command}) => {
+  const config = {
+    plugins: [react()],
+    base: "/", // for github pages I GUESS
+    // base: "/jattiman.github.io/", // for github pages I GUESS
+  }
+  if (command !== 'serve') {
+    config.base = '/';
+  }
+  return config;
+
 })
