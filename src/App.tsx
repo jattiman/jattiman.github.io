715ebe1b13b4e2bac6e6a3a527e2c2e5223f36bf
John Atti <33670809+jattiman@users.noreply.github.com>
2023-06-25 20:18:01 -0400

    I am so smrt!
    I am so smart! I am so smart! S-M-R-T! ... S-M-A-R-T!

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,64 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+  const CounterThing = () =>{
+
+    const TimeIsComplicated = () => {
+      if(count === 1) {
+        return "time";
+      }
+      else{
+        return "times";
+      }
+    }
+
+    const TimeIsReallyComplicated = () => {
+      switch(true){
+        case (count <= 9):
+          return "";
+        case (count <= 20):
+          return(
+              <>
+                <p>There is no reward for clicking this button more, aside from changing text.</p>
+              </>
+            );
+        case (count <= 30):
+          return(
+            <>
+              <p>I'll only be updating the text one more time.</p>
+            </>
+          );
+        case (count <= 100):
+          return(
+            <>
+              <p>Have a good day.</p>
+            </>
+          );
+        default:
+          return "";
+      }
+    }
+
+    return (
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Made silly mistakes {count} <TimeIsComplicated/> while creating this site. <TimeIsReallyComplicated/>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    )
+  }
+
+  return (
+    <>
+      <p className="main-title">
+        Coming in 2023. Honest.
+      </p>
+      <CounterThing />
+      <p className="ugly-gray-text">
+        Click clack.
       </p>
     </>
   )
